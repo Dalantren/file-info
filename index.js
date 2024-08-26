2ea6f109ce7df10b8b8ade1fe928a8286b71f261
Ivanitskii Artem <artem.ivanitskii@mlp.com>
2024-08-27 00:42:24 +0200

    1

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var csv_stringify_1 = require("csv-stringify");
-console.log('Hello world!');
+var gracefulFs = require("graceful-fs");
+gracefulFs.gracefulify(fs);
 var columns = {
     fileName: 'File Name',
     size: 'Size, bytes',
+    kbSize: 'Size, kB',
     mbSize: 'Size, MB',
     gbSize: 'Size, GB',
 };
@@ -18,21 +20,31 @@ fs.readdir(__dirname, function (err, fileNames) {
     if (err) {
         return;
     }
-    console.log(fileNames);
+    var total = {
+        bytes: 0,
+        kB: 0,
+        mB: 0,
+        gB: 0,
+    };
     var filesInfo = fileNames.map(function (fileName) {
         var size = getFilesize("./".concat(fileName));
-        var mbSize = size / (1024 * 1024);
+        var kbSize = size / 1024;
+        var mbSize = kbSize / 1024;
         var gbSize = mbSize / 1024;
-        // console.log(fileName, getFilesizeInMb(`./${fileName}`));
-        return { fileName: fileName, size: size, mbSize: "".concat(mbSize.toFixed(2), " Mb"), gbSize: "".concat(gbSize.toFixed(2), " Gb") };
+        total.bytes += size;
+        total.kB += kbSize;
+        total.mB += mbSize;
+        total.gB += gbSize;
+        return { fileName: fileName, size: size, kbSize: "".concat(kbSize.toFixed(2), " kB"), mbSize: "".concat(mbSize.toFixed(2), " Mb"), gbSize: "".concat(gbSize.toFixed(2), " Gb") };
     });
     (0, csv_stringify_1.stringify)(filesInfo, { header: true, columns: columns }, function (err, output) {
         if (err)
             throw err;
+        output += '\n';
+        output += "Total,".concat(total.bytes, ",").concat(total.kB.toFixed(2), " kB,").concat(total.mB.toFixed(2), " Mb,").concat(total.gB.toFixed(2), " Gb");
         fs.writeFile('my.csv', output, function (err) {
             if (err)
                 throw err;
-            console.log('my.csv saved.');
         });
     });
 });
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import * as fs from 'fs';
 import { stringify } from 'csv-stringify';
-
-console.log('Hello world!');
+import * as gracefulFs from 'graceful-fs';
+gracefulFs.gracefulify(fs);
 
 const columns = {
   fileName: 'File Name',
   size: 'Size, bytes',
+  kbSize: 'Size, kB',
   mbSize: 'Size, MB',
   gbSize: 'Size, GB',
 };
@@ -20,21 +21,32 @@ fs.readdir(__dirname, (err, fileNames) => {
   if (err) {
     return;
   }
-  console.log(fileNames);
+
+  let total = {
+    bytes: 0,
+    kB: 0,
+    mB: 0,
+    gB: 0,
+  };
 
   const filesInfo = fileNames.map(fileName => {
     const size = getFilesize(`./${fileName}`);
-    const mbSize = size / (1024 * 1024);
+    const kbSize = size / 1024;
+    const mbSize = kbSize / 1024;
     const gbSize = mbSize / 1024;
-    // console.log(fileName, getFilesizeInMb(`./${fileName}`));
-    return { fileName, size, mbSize: `${mbSize.toFixed(2)} Mb`, gbSize: `${gbSize.toFixed(2)} Gb` };
+    total.bytes += size;
+    total.kB += kbSize;
+    total.mB += mbSize;
+    total.gB += gbSize;
+    return { fileName, size, kbSize: `${kbSize.toFixed(2)} kB`, mbSize: `${mbSize.toFixed(2)} Mb`, gbSize: `${gbSize.toFixed(2)} Gb` };
   })
 
   stringify(filesInfo, { header: true, columns: columns }, (err, output) => {
     if (err) throw err;
+    output += '\n';
+    output += `Total,${total.bytes},${total.kB.toFixed(2)} kB,${total.mB.toFixed(2)} Mb,${total.gB.toFixed(2)} Gb`;
     fs.writeFile('my.csv', output, (err) => {
       if (err) throw err;
-      console.log('my.csv saved.');
     });
   });
 })
